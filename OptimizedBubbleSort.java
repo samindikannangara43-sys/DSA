cf1c26f9210f78bd45fa5d29c5bfe6a6ffa86070
samindikannangara43-sys <ranuthkannangara43@gmail.com>
2026-01-13 21:36:43 +0530

    Add files via upload

---
+++ OptimizedBubbleSort.java
@@ -0,0 +1,35 @@
+class OptimizedBubbleSort {
+
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped)
+                break;
+        }
+    }
+
+    static void display(int[] arr) {
+        for (int x : arr)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        bubbleSort(arr);
+        display(arr);
+    }
+}
---
+++ insertionSort.java
@@ -0,0 +1,30 @@
+class InsertionSort {
+
+    static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    static void display(int[] arr) {
+        for (int x : arr)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 5, 1, 4, 3};
+        insertionSort(arr);
+        display(arr);
+    }
+}
